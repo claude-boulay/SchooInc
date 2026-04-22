6b9043465a38577791cdb8d716fe83820b672278
StevenCsth <coublants@gmail.com>
2026-04-22 15:11:03 +0200

    del: pages en trop

--- Frontend/src/pages/ProfessorClassesPage.jsx
+++
@@ -1,9 +0,0 @@
-import PageSection from '../components/PageSection'
-
-export default function ProfessorClassesPage() {
-    return (
-        <PageSection title="Gestion des classes" subtitle="Creation, modification, suppression et gestion des etudiants.">
-            <p>Actions reservees au professeur responsable des classes ciblees.</p>
-        </PageSection>
-    )
-}
--- Frontend/src/pages/ProfessorCoursesPage.jsx
+++
@@ -1,9 +0,0 @@
-import PageSection from '../components/PageSection'
-
-export default function ProfessorCoursesPage() {
-    return (
-        <PageSection title="Gestion des cours" subtitle="Creation, modification et suppression de cours.">
-            <p>Cette page doit gerer uniquement les cours du professeur connecte.</p>
-        </PageSection>
-    )
-}
