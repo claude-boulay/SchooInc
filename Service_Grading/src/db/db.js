5a34b6a4a0ce1e8f8a306358cdc6f597ae665824
Boulay <cbmagala@gmail.com>
2026-04-23 08:51:57 +0200

    final commit

--- Service_Grading/src/db/db.js
+++ Service_Grading/src/db/db.js
@@ -7,8 +7,6 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD || "grading_pass",
   database: process.env.DB_NAME || "grading_db",
   max: Number(process.env.DB_POOL_MAX || 10),
-  idleTimeoutMillis: Number(process.env.DB_IDLE_TIMEOUT_MS || 30000),
-  connectionTimeoutMillis: Number(process.env.DB_CONNECTION_TIMEOUT_MS || 5000),
 });
 
 pool.on("error", (error) => {
--- Service_User/src/db/db.js
+++ Service_User/src/db/db.js
@@ -7,8 +7,6 @@ const pool = new Pool({
 	password: process.env.DB_PASSWORD || "postgres",
 	database: process.env.DB_NAME || "schoolinc_users",
 	max: Number(process.env.DB_POOL_MAX || 10),
-	idleTimeoutMillis: Number(process.env.DB_IDLE_TIMEOUT_MS || 30000),
-	connectionTimeoutMillis: Number(process.env.DB_CONNECTION_TIMEOUT_MS || 5000),
 });
 
 pool.on("error", (error) => {
